3e5c8a148be59e72a58f040313ba00438bc0d21d
Kasualix <670067575@qq.com>
2023-06-20 21:01:36 +0800

    cleanup

--- src/main/java/com/teampotato/keepcuriosinventory/KeepCuriosInventory.java
+++ src/main/java/com/teampotato/keepcuriosinventory/KeepCuriosInventory.java
@@ -1,17 +1,8 @@
 package com.teampotato.keepcuriosinventory;
 
-import net.minecraft.world.entity.player.Player;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import top.theillusivec4.curios.api.event.CurioDropsEvent;
 
 @Mod(KeepCuriosInventory.ID)
-@Mod.EventBusSubscriber()
 public class KeepCuriosInventory {
     public static final String ID = "keepcuriosinventory";
-
-    @SubscribeEvent
-    public static void onCuriosDrop(CurioDropsEvent event) {
-        if (event.getEntity() instanceof Player) event.setCanceled(true);
-    }
 }
