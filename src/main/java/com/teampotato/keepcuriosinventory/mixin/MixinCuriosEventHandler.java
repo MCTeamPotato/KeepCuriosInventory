629dc885eb541e3ebb58dd47892af6e2e689b2c9
Kasualix <670067575@qq.com>
2023-06-22 04:44:41 +0800

    port

--- src/main/java/com/teampotato/keepcuriosinventory/mixin/MixinCuriosEventHandler.java
+++ src/main/java/com/teampotato/keepcuriosinventory/mixin/MixinCuriosEventHandler.java
@@ -1,10 +1,9 @@
 package com.teampotato.keepcuriosinventory.mixin;
 
-import net.minecraft.core.NonNullList;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.item.ItemEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.Tuple;
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.item.ItemEntity;
-import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.living.LivingDropsEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
@@ -16,58 +15,52 @@ import top.theillusivec4.curios.api.CuriosApi;
 import top.theillusivec4.curios.api.event.CurioDropsEvent;
 import top.theillusivec4.curios.api.event.DropRulesEvent;
 import top.theillusivec4.curios.api.type.capability.ICurio;
-import top.theillusivec4.curios.api.type.inventory.ICurioStacksHandler;
 import top.theillusivec4.curios.api.type.inventory.IDynamicStackHandler;
 import top.theillusivec4.curios.common.event.CuriosEventHandler;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-import java.util.Map;
 import java.util.function.Predicate;
 
 @Mixin(value = CuriosEventHandler.class, remap = false)
 public abstract class MixinCuriosEventHandler {
     @Shadow
-    private static void handleDrops(String identifier, LivingEntity livingEntity, List<Tuple<Predicate<ItemStack>, ICurio.DropRule>> dropRules, NonNullList<Boolean> renders, IDynamicStackHandler stacks, boolean cosmetic, Collection<ItemEntity> drops, boolean keepInventory, LivingDropsEvent evt) {}
+    private static void handleDrops(LivingEntity livingEntity, List<Tuple<Predicate<ItemStack>, ICurio.DropRule>> dropRules, IDynamicStackHandler stacks, Collection<ItemEntity> drops, boolean keepInventory) {}
 
     /**
      * @author Kasualix
      * @reason avoid drop
      */
     @Overwrite
-    @SubscribeEvent(priority = EventPriority.HIGHEST)
+    @SubscribeEvent(
+            priority = EventPriority.HIGHEST
+    )
     public void playerDrops(LivingDropsEvent evt) {
-
-        LivingEntity livingEntity = evt.getEntity();
-
+        LivingEntity livingEntity = evt.getEntityLiving();
         if (!livingEntity.isSpectator()) {
-
-            CuriosApi.getCuriosHelper().getCuriosHandler(livingEntity).ifPresent(handler -> {
-                Collection<ItemEntity> drops = evt.getDrops();
+            CuriosApi.getCuriosHelper().getCuriosHandler(livingEntity).ifPresent((handler) -> {
                 Collection<ItemEntity> curioDrops = new ArrayList<>();
-                Map<String, ICurioStacksHandler> curios = handler.getCurios();
-
-                DropRulesEvent dropRulesEvent = new DropRulesEvent(livingEntity, handler, evt.getSource(),
-                        evt.getLootingLevel(), evt.isRecentlyHit());
+                DropRulesEvent dropRulesEvent = new DropRulesEvent(livingEntity, handler, evt.getSource(), evt.getLootingLevel(), evt.isRecentlyHit());
                 MinecraftForge.EVENT_BUS.post(dropRulesEvent);
                 List<Tuple<Predicate<ItemStack>, ICurio.DropRule>> dropRules = dropRulesEvent.getOverrides();
-
                 boolean keepInventory = true;
-
-                curios.forEach((id, stacksHandler) -> {
-                    handleDrops(id, livingEntity, dropRules, stacksHandler.getRenders(),
-                            stacksHandler.getStacks(), false, curioDrops, keepInventory, evt);
-                    handleDrops(id, livingEntity, dropRules, stacksHandler.getRenders(),
-                            stacksHandler.getCosmeticStacks(), true, curioDrops, keepInventory, evt);
+                handler.getCurios().forEach((id, stacksHandler) -> {
+                    handleDrops(livingEntity, dropRules, stacksHandler.getStacks(), curioDrops, keepInventory);
+                    handleDrops(livingEntity, dropRules, stacksHandler.getCosmeticStacks(), curioDrops, keepInventory);
                 });
-
-                if (!MinecraftForge.EVENT_BUS.post(
-                        new CurioDropsEvent(livingEntity, handler, evt.getSource(), curioDrops,
-                                evt.getLootingLevel(), evt.isRecentlyHit()))) {
-                    drops.addAll(curioDrops);
+                if (!MinecraftForge.EVENT_BUS.post(new CurioDropsEvent(
+                        livingEntity,
+                        handler,
+                        evt.getSource(),
+                        curioDrops,
+                        evt.getLootingLevel(),
+                        evt.isRecentlyHit()
+                ))) {
+                    evt.getDrops().addAll(curioDrops);
                 }
             });
         }
+
     }
 }
